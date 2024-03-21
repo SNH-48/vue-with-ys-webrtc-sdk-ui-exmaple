58db2c82d15e5b688c1ad8294a0bb6c331cfbcfb
chenwt <chenwt@yeastar.com>
2024-03-21 19:13:30 +0800

    feat: init repo

---
+++ README.md
@@ -0,0 +1,22 @@
+# WebRTC-SDK Demo
+
+工程模板：Vue 3 + TypeScript + Vite  
+
+用于演示 WebRTC-SDK 的使用方法。同时也用于测试 WebRTC-SDK 的功能。
+## 项目运行
+```bash
+npm run dev
+```
+## 项目打包
+```bash
+npm run build
+```
+## 项目发布
+发布至192.168.12.13服务器  
+
+服务器ssh web web123456
+
+服务静态资源目录 /home/webRTC/dist
+
+## 注意事项
+每次发布前必须修改公告牌，公告牌组件在`src/components/Billboard.vue`中。
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="testRef" id="test">
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+// @ts-ignore
+import { init, on } from 'ys-webrtc-sdk-ui';
+import 'ys-webrtc-sdk-ui/lib/ys-webrtc-sdk-ui.css';
+const testRef = ref(null);
+onMounted(()=>{
+  if (testRef.value) {
+    init(testRef.value, {
+      pbxURL: 'https://your-pbx.com',
+      secret: '',
+      username: '1000',
+    }).then(()=>{
+      console.log('init success')
+    })
+  }
+});
+</script>
